9c2903484df2fdcafb542d8c3e8cf364b5395940
Marcello Tedardi <mvtedardi@gmail.com>
2018-12-05 11:20:55 -0200

    O codigo 'code_wifi_ns3.cc' esta compilando

--- src/code_wifi_ns3.cc
+++ src/code_wifi_ns3.cc
@@ -10,15 +10,29 @@
 #include "ns3/flow-monitor-module.h"
 #include "ns3/flow-monitor-helper.h"
 #include "ns3/olsr-helper.h"
+#include "ns3/athstats-helper.h"
+#include "ns3/config-store-module.h"
+#include "ns3/packet-socket-helper.h"
+#include "ns3/packet-socket-address.h"
+#include "ns3/on-off-helper.h"
+#include "ns3/command-line.h"
+#include "ns3/config.h"
+#include "ns3/boolean.h"
+#include "ns3/yans-wifi-helper.h"
+#include "ns3/yans-wifi-channel.h"
 #include "stdlib.h"
-include <iostream>
+#include <iostream>
 #include <sstream>
 #include <fstream>
 #include <cstdlib>
 #include <string>
 
+/*
+* Nesse caso ele solicita que seja usa
+* Referencia: https://stackoverflow.com/questions/5499189/stdstring-vs-string-in-c
+*/
 
-
+using std::string;
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE ("Running Code Wifi NS3");
@@ -44,11 +58,11 @@ wifiPhy1.SetChannel(wifiChannel.Create());
 WifiHelper wifiAP = WifiHelper::Default();
 string phyMode("DsssRate1Mbps");
 
-NetDevice Container apWifiDevs;
+NetDeviceContainer apWifiDevs;
 wifiAP.SetStandard(WIFI_PHY_STANDARD_80211g);
 wifiAP.SetRemoteStationManager("ns3::ConstantRateWifiManager", "DataMode", StringValue (phyMode), "ControlMode", StringValue(phyMode));
 
-apWifiDevs.Add(wifiAp.Install(wifiPhy1, wifiMac, aps));
+apWifiDevs.Add(wifiAP.Install(wifiPhy1, wifiMac, aps));
 
 WifiHelper wifi = WifiHelper::Default();
 wifi.SetStandard(WIFI_PHY_STANDARD_80211g);
